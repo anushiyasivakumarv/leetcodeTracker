1befb864735281d4c8b850f5721198cf8343752b
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-07-09 09:16:51 +0530

    Create 1746-LargestSubstringBetweenTwoEqualCharacters

---
+++ 1746-LargestSubstringBetweenTwoEqualCharacters/1746-LargestSubstringBetweenTwoEqualCharacters.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 09:16:53
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        int ans = -1;
+        for (int left = 0; left < s.length(); left++) {
+            for (int right = left + 1; right < s.length(); right++) {
+                if (s.charAt(left) == s.charAt(right)) {
+                    ans = Math.max(ans, right - left - 1);
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
