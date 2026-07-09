53a4a062ef0ac4b253cd52dfa1779db5ce94f207
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-07-09 09:18:34 +0530

    Create 189-RotateArray

---
+++ 189-RotateArray/189-RotateArray.java
@@ -0,0 +1,16 @@
+// Last updated: 09/07/2026, 09:18:36
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;  
+        int[] temp = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
