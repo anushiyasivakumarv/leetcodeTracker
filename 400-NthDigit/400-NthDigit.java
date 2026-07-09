c2bbfde966124711bd8119d19ce008b87e8daee9
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-07-09 09:17:57 +0530

    Create 400-NthDigit

---
+++ 400-NthDigit/400-NthDigit.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:17:59
+class Solution {
+    public int findNthDigit(int n){
+        long digitlength = 1;
+        int start = 1;
+        int end = 9;
+        while(n > digitlength*end){
+            n -= digitlength*end;
+            digitlength++;
+            start*=10;
+            end*=10;
+        }
+        start += (n-1)/digitlength;
+        String number = String.valueOf(start); 
+        int index = (int) ((n - 1) % digitlength);  
+        return number.charAt(index) - '0';
+    
+    }
+}
