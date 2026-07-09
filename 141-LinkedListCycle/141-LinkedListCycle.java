269bb3e9c7b7b8ada5a33cbaebb26c4066b9333a
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-07-09 09:18:46 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -0,0 +1,13 @@
+// Last updated: 09/07/2026, 09:18:48
+public class Solution{
+    public boolean hasCycle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
