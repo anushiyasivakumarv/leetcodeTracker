a31a88e1099ee5e8f0a4a020286cf6922acd8a57
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-07-09 09:18:36 +0530

    Create 179-LargestNumber

---
+++ 179-LargestNumber/179-LargestNumber.java
@@ -0,0 +1,19 @@
+// Last updated: 09/07/2026, 09:18:38
+class Solution {
+
+    public String largestNumber(int[] nums) {
+        String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+        String result = "";
+        for (String s : arr) {
+            result += s;
+        }
+        return result;
+    }
+}
