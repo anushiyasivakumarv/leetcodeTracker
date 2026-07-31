1917e754426b16f679a2e4e143f0b6e531f5424e
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-07-31 14:36:35 +0530

    Create 377-CombinationSumIv

---
+++ 377-CombinationSumIv/377-CombinationSumIv.java
@@ -0,0 +1,11 @@
+// Last updated: 7/31/2026, 2:36:35 PM
+class Solution {
+    public int combinationSum4(int[] N, int T) {
+        int[] dp = new int[T+1];
+        dp[0] = 1;
+        for (int i = 1; i <= T; i++)
+            for (int num : N)
+                if (num <= i) dp[i] += dp[i-num];
+        return dp[T];
+    }
+}
