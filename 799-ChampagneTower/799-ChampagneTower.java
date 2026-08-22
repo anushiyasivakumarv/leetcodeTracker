b1fdf8f859aefbbc51d8747a3cde6a87409eb2bc
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-08-22 16:20:03 +0530

    File updated at 8/22/2026, 4:20:03 PM

--- 799-ChampagneTower/799-ChampagneTower.java
+++ 799-ChampagneTower/799-ChampagneTower.java
@@ -1,19 +1,24 @@
-// Last updated: 8/22/2026, 4:10:47 PM
+// Last updated: 8/22/2026, 4:20:03 PM
 1class Solution {
-2    public List<Integer> addToArrayForm(int[] num, int k) {
-3        List<Integer> result=new ArrayList<>();
-4        int n=num.length;
-5
-6        for(int i=n-1;i>=0;i--){
-7            k+=num[i];
-8            result.add(k%10); 
-9            k/=10; 
-10        }
-11        while(k>0){
-12            result.add(k%10);
-13            k/=10;
-14        }
-15        Collections.reverse(result);
-16        return result;
-17    }
-18}
+2    public int maxNumberOfBalloons(String text) {
+3        int[] fr = new int[26];
+4
+5        for (char ch : text.toCharArray()) {
+6            fr[ch - 'a']++;
+7        }
+8
+9        String balloon = "balloon";
+10        int count = 0;
+11
+12        while (true) {
+13            
+14            for (char ch : balloon.toCharArray()) {
+15                if (fr[ch - 'a'] == 0) {
+16                    return count;
+17                }
+18                fr[ch - 'a']--;
+19            }
+20            count++;
+21        }
+22    }
+23}
