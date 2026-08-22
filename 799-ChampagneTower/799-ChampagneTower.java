729212d0924d5b5327d138085e7a73c42ba2472b
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-08-22 17:36:45 +0530

    File updated at 8/22/2026, 5:36:46 PM

--- 799-ChampagneTower/799-ChampagneTower.java
+++ 799-ChampagneTower/799-ChampagneTower.java
@@ -1,28 +1,29 @@
-// Last updated: 8/22/2026, 4:26:48 PM
+// Last updated: 8/22/2026, 5:36:46 PM
 1class Solution {
-2    public int nthUglyNumber(int n, int a, int b, int c) {
-3        int left = 1;
-4        int right = Integer.MAX_VALUE;
-5        int count = 0;
-6        while (left < right) {
-7            int middle = left + (right - left) / 2;
-8            if (isUgly(middle, a, b, c, n)) {
-9                right = middle;
-10            }
-11            else
-12                left = middle + 1;
-13        }
-14        return left;
-15    }
-16    public boolean isUgly(long middle, long a, long b, long c, long n) {
-17        return (int) (middle/a + middle/b + middle/c - middle/lcm(a, b) - middle/lcm(b, c) - middle/lcm(c, a) + middle/lcm(a, lcm(b, c))) >= n;
-18    }
-19    public long gcd(long a, long b) {
-20        if (a == 0)
-21            return b;
-22        else return gcd(b%a, a);
-23    }
-24    public long lcm(long a, long b) {
-25        return a * b / (gcd(a, b)); 
-26    }
-27}
+2    public boolean isPossibleDivide(int[] nums, int k) {
+3        if (nums.length % k != 0) {
+4            return false;
+5        }
+6
+7        Map<Integer, Integer> map = new HashMap<>();
+8        for (int num : nums) {
+9            map.put(num, map.getOrDefault(num, 0) + 1);
+10        }
+11
+12        Arrays.sort(nums);
+13
+14        for (int num : nums) {
+15            if (map.get(num) > 0) {
+16                for (int i = num + 1; i < num + k; i++) {
+17                    if (map.getOrDefault(i, 0) == 0) {
+18                        return false;
+19                    }
+20                    map.put(i, map.get(i) - 1);
+21                }
+22                map.put(num, map.get(num) - 1);
+23            }
+24        }
+25
+26        return true;
+27    }
+28}
