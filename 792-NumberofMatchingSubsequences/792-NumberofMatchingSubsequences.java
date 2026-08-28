a843be13a62b743a9761ca8af9d2b894e329d42d
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-08-28 14:32:50 +0530

    File updated at 8/28/2026, 2:32:52 PM

--- 792-NumberofMatchingSubsequences/792-NumberofMatchingSubsequences.java
+++ 792-NumberofMatchingSubsequences/792-NumberofMatchingSubsequences.java
@@ -1,29 +1,11 @@
-// Last updated: 8/28/2026, 2:24:54 PM
+// Last updated: 8/28/2026, 2:32:52 PM
 1class Solution {
-2    public String reorderSpaces(String text) {
-3        int totalSpaces = 0;
-4        for (char c : text.toCharArray()) {
-5            if (c == ' ') totalSpaces++;
-6        }
-7        String[] words = text.trim().split("\\s+");
-8        int wordCount = words.length;
-9        int spacesBetween = 0;
-10        int extraSpaces = 0;
-11        if (wordCount > 1) {
-12            spacesBetween = totalSpaces / (wordCount - 1);
-13            extraSpaces = totalSpaces % (wordCount - 1);
-14        } else {
-15            extraSpaces = totalSpaces;
-16        }
-17        StringBuilder result = new StringBuilder();
-18        for (int i = 0; i < wordCount; i++) {
-19            result.append(words[i]);
-20            if (i < wordCount - 1) {
-21                result.append(" ".repeat(spacesBetween));
-22            }
-23        }
-24        result.append(" ".repeat(extraSpaces));
-25
-26        return result.toString();
-27    }
-28}
+2    public int[] runningSum(int[] nums) {
+3       int[] rs = new int[nums.length];
+4       rs[0] = nums[0];
+5        for (int i = 1; i < nums.length; i++) {
+6            rs[i] = rs[i - 1] + nums[i];
+7        }
+8        return rs;
+9    }
+10}
