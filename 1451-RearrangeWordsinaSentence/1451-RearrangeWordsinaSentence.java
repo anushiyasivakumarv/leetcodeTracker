8c63753d05d9471d197b17f61d51c6927956d3e4
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-09-01 15:10:22 +0530

    File updated at 9/1/2026, 3:10:23 PM

--- 1451-RearrangeWordsinaSentence/1451-RearrangeWordsinaSentence.java
+++ 1451-RearrangeWordsinaSentence/1451-RearrangeWordsinaSentence.java
@@ -1,10 +1,12 @@
-// Last updated: 9/1/2026, 2:56:25 PM
+// Last updated: 9/1/2026, 3:10:23 PM
 1class Solution {
-2    public String arrangeWords(String text) {
-3        String[] words = text.split(" ");
-4        words[0] = words[0].toLowerCase();
-5        Arrays.sort(words, Comparator.comparingInt(String::length));
-6        words[0] = words[0].substring(0, 1).toUpperCase() + words[0].substring(1);
-7        return String.join(" ", words);
-8    }
-9}
+2    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+3        int count = 0;
+4        for(int i = 0; i < startTime.length; i++){
+5            if(startTime[i] <= queryTime && queryTime <= endTime[i]){
+6                count++;
+7            }
+8        }
+9        return count;
+10    }
+11}
