7d8a86b6ceec11268c3f3221591e179bfb998179
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-09-02 09:12:18 +0530

    Create 372-SuperPow

---
+++ 372-SuperPow/372-SuperPow.java
@@ -0,0 +1,17 @@
+// Last updated: 9/2/2026, 9:12:19 AM
+import java.math.BigInteger;
+
+class Solution {
+    public int superPow(int a, int[] b) {
+      StringBuilder str=new StringBuilder();
+      for(int i=0; i<b.length; i++)
+      {
+        str.append(Integer.toString(b[i]));
+      }
+      String st=str.toString();
+      BigInteger exponent=new BigInteger(st);
+      BigInteger base=BigInteger.valueOf(a);
+      BigInteger ans=base.modPow(exponent, BigInteger.valueOf(1337));
+      return ans.intValue();
+    }
+}
