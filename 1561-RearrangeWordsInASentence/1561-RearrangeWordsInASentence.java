b71e35594cd8df010cd265bd6a826ab956c9f191
anushiyasivakumarv <anushiyasivakumarv@gmail.com>
2026-09-02 09:10:41 +0530

    Create 1561-RearrangeWordsInASentence

---
+++ 1561-RearrangeWordsInASentence/1561-RearrangeWordsInASentence.java
@@ -0,0 +1,10 @@
+// Last updated: 9/2/2026, 9:10:41 AM
+class Solution {
+    public String arrangeWords(String text) {
+        String[] words = text.split(" ");
+        words[0] = words[0].toLowerCase();
+        Arrays.sort(words, Comparator.comparingInt(String::length));
+        words[0] = words[0].substring(0, 1).toUpperCase() + words[0].substring(1);
+        return String.join(" ", words);
+    }
+}
