01c4d777b445761dd95d8ab2a3eb22b2b5defe00
ANUSHIYA S <anushiyasivakumarv@gmail.com>
2026-09-09 15:53:43 +0530

    File updated at 9/9/2026, 3:53:43 PM

--- 1352-ProductoftheLastKNumbers/1352-ProductoftheLastKNumbers.java
+++ 1352-ProductoftheLastKNumbers/1352-ProductoftheLastKNumbers.java
@@ -1,11 +1,20 @@
-// Last updated: 9/9/2026, 3:11:49 PM
-1import java.time.LocalDate;
-2import java.time.temporal.ChronoUnit;
+// Last updated: 9/9/2026, 3:53:43 PM
+1class Solution {
+2    public int maxProduct(int[] nums) {
 3
-4class Solution {
-5    public int daysBetweenDates(String date1, String date2) {
-6        LocalDate d1 = LocalDate.parse(date1);
-7        LocalDate d2 = LocalDate.parse(date2);
-8        return (int) Math.abs(ChronoUnit.DAYS.between(d1, d2));
-9    }
-10}
+4        int max1 = Integer.MIN_VALUE;
+5        int max2 = Integer.MIN_VALUE;
+6
+7        for (int num : nums) {
+8
+9            if (num > max1) {
+10                max2 = max1;
+11                max1 = num;
+12            } else if (num > max2) {
+13                max2 = num;
+14            }
+15        }
+16
+17        return (max1 - 1) * (max2 - 1);
+18    }
+19}
